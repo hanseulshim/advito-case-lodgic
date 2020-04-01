5ba2dcd00b0c749609aba6d71adf5277d6ed092f
Hanseul Shim <hanseulshim@gmail.com>
2020-04-01 16:55:57 -0400

    Prepare seed data

--- server/db/seeds/addJobIngestionHotels.js
+++ server/db/seeds/addJobIngestionHotels.js
@@ -1,3 +1,26 @@
+const faker = require('faker')
+
+const jobIdList = [282, 280, 271]
+
+// const createRow = () => ({
+//   "job_ingestion_id": jobIdList[Math.floor(Math.random()*3)],
+//   "ingestion_type" varchar(255) COLLATE "pg_catalog"."default",
+//   "original_unmatched_count" int4,
+//   "original_unmatched_spend_usd" numeric(16,4),
+//   "unmatched_count" int4,
+//   "unmatched_spend_usd" numeric(16,4),
+//   "matched_count" int4,
+//   "matched_spend_usd" numeric(16,4),
+//   "ingestion_status" varchar(255) COLLATE "pg_catalog"."default",
+//   "is_dpm" bool NOT NULL,
+//   "status_dpm" varchar(255) COLLATE "pg_catalog"."default",
+//   "date_status_dpm" timestamp(6),
+//   "is_sourcing" bool NOT NULL,
+//   "status_sourcing" varchar(255) COLLATE "pg_catalog"."default",
+//   "date_status_sourcing" timestamp(6),
+//   "ingestion_note" text COLLATE "pg_catalog"."default",
+// })
+
 exports.seed = knex => {
 	// Deletes ALL existing entries
 	return knex('job_ingestion_hotel')
