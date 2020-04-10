fcad2875dc66adea2e534bef6e0a88b752013a32
Hanseul Shim <hanseulshim@gmail.com>
2020-04-10 17:23:05 -0400

    UPdate seeds

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,9 +1,23 @@
 const yenv = require('yenv')
 
-const env = yenv('../env.yml', { env: 'dev' })
+const env = yenv('../env.yml', {
+	env: process.env.npm_config_argv.includes('staging') ? 'staging' : 'dev'
+})
 
 module.exports = {
 	development: {
+		client: 'pg',
+		connection: {
+			host: env.DB_HOST,
+			user: env.DB_USER,
+			password: env.DB_PASSWORD,
+			database: env.DB_NAME
+		},
+		seeds: {
+			directory: __dirname + '/seeds'
+		}
+	},
+	staging: {
 		client: 'pg',
 		connection: {
 			host: env.DB_HOST,
