6e2e22f43edb450a7774a8dfddf5ab143fa8eaa5
Hanseul Shim <hanseulshim@gmail.com>
2020-04-16 23:39:54 -0400

    Add eslint warning disable

--- server/src/types/context.ts
+++ server/src/types/context.ts
@@ -3,6 +3,7 @@ import Knex from 'knex/types/index'
 
 export type ContextType = {
 	user: UserType
+	/* eslint-disable  @typescript-eslint/no-explicit-any */
 	advito: Knex<any, unknown[]>
 	hotel: Knex<any, unknown[]>
 }
