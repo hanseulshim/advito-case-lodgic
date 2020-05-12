fb0f7e6827c61b68f5c10c67d013f9e279ab45e6
joneil@boostlabs.com <joneil@boostlabs.com>
2020-05-12 15:23:00 -0500

    clear match on record change

--- client/src/components/main/work-record/index.js
+++ client/src/components/main/work-record/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import RecordHeader from './RecordHeader'
 import MatchesTable from './MatchesTable'
@@ -9,6 +9,11 @@ const WorkRecord = () => {
 	const recordId = record.split('-')[0]
 	const recordIndex = record.split('-')[1]
 	const [matchedHotel, setMatchedHotel] = useState(null)
+
+	useEffect(() => {
+		//Reset matched hotel if user goes to a new record
+		setMatchedHotel(null)
+	}, [record])
 
 	const onMatchHotel = (record) => {
 		if (record === matchedHotel) {
