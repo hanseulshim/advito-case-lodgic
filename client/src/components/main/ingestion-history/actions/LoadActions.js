da88f3a55892e09725a2230db2c1a05f2ebd25fe
joneil@boostlabs.com <joneil@boostlabs.com>
2020-06-12 12:27:04 -0500

    cleanup columns

--- client/src/components/main/ingestion-history/actions/LoadActions.js
+++ client/src/components/main/ingestion-history/actions/LoadActions.js
@@ -3,15 +3,20 @@ import { Checkbox } from 'antd'
 
 const LoadActions = ({ record, selectedRecords, setSelectedRecords }) => {
 	const onChange = (type) => {
+		//Check if record is already selected
 		const index = selectedRecords.findIndex(
 			(selected) => selected.jobIngestionId === record.jobIngestionId
 		)
+
+		//If record is not selected, add it to selectedRecords
 		if (index === -1) {
 			setSelectedRecords([
 				...selectedRecords,
 				{ type, jobIngestionId: record.jobIngestionId, jobName: record.jobName }
 			])
-		} else if (index !== -1 && selectedRecords[index].type !== type) {
+		}
+		// If record is selected but it has a different type, change the type
+		else if (index !== -1 && selectedRecords[index].type !== type) {
 			const filtered = selectedRecords.filter(
 				(selectedRecord) =>
 					selectedRecord.jobIngestionId !== record.jobIngestionId
@@ -21,7 +26,9 @@ const LoadActions = ({ record, selectedRecords, setSelectedRecords }) => {
 				type
 			}
 			setSelectedRecords([{ ...updated }, ...filtered])
-		} else {
+		}
+		// If record is already there with same type, remove it
+		else {
 			const filtered = selectedRecords.filter(
 				(selectedRecord) =>
 					selectedRecord.jobIngestionId !== record.jobIngestionId
@@ -31,14 +38,20 @@ const LoadActions = ({ record, selectedRecords, setSelectedRecords }) => {
 	}
 
 	const { isDpm, statusDpm, isSourcing, statusSourcing } = record
-	const displayArr = []
-	if (
-		!isDpm ||
-		(!isDpm && isSourcing && statusSourcing.toLowerCase() === 'approved')
-	) {
-		displayArr.push(
+
+	return (
+		<div style={{ display: 'flex', justifyContent: 'space-between' }}>
 			<Checkbox
 				onChange={() => onChange('DPM')}
+				style={{
+					visibility:
+						!isDpm ||
+						(!isDpm &&
+							isSourcing &&
+							statusSourcing.toLowerCase() === 'approved')
+							? ''
+							: 'hidden'
+				}}
 				checked={selectedRecords.some(
 					(selectedRecord) =>
 						selectedRecord.jobIngestionId === record.jobIngestionId &&
@@ -47,15 +60,15 @@ const LoadActions = ({ record, selectedRecords, setSelectedRecords }) => {
 			>
 				DPM
 			</Checkbox>
-		)
-	}
-	if (
-		!isSourcing ||
-		(!isSourcing && isDpm && statusDpm.toLowerCase() === 'approved')
-	) {
-		displayArr.push(
 			<Checkbox
 				onChange={() => onChange('Sourcing')}
+				style={{
+					visibility:
+						!isSourcing ||
+						(!isSourcing && isDpm && statusDpm.toLowerCase() === 'approved')
+							? ''
+							: 'hidden'
+				}}
 				checked={selectedRecords.some(
 					(selectedRecord) =>
 						selectedRecord.jobIngestionId === record.jobIngestionId &&
@@ -64,10 +77,8 @@ const LoadActions = ({ record, selectedRecords, setSelectedRecords }) => {
 			>
 				Sourcing
 			</Checkbox>
-		)
-	}
-
-	return displayArr.length ? displayArr.map((el, i) => ({ ...el, key: i })) : []
+		</div>
+	)
 }
 
 export default LoadActions
--- client/src/components/main/ingestion-history/index.js
+++ client/src/components/main/ingestion-history/index.js
@@ -14,6 +14,7 @@ import ActivityDataQc from './actions/ActivityDataQc'
 import ApproveSourcing from './actions/ApproveSourcing'
 import ApproveDPM from './actions/ApproveDPM'
 import LoadEnhancedQc from './actions/LoadEnhancedQc'
+import './columns.scss'
 
 const ButtonRow = styled.div`
 	display: flex;
