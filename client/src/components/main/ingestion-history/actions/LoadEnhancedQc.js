e1b31dc0bf764d277c6ac449cf08cc8f182fb551
joneil@boostlabs.com <joneil@boostlabs.com>
2020-06-17 13:24:13 -0500

    clean loading before polling refactor

--- client/src/components/main/ingestion-history/actions/LoadEnhancedQc.js
+++ client/src/components/main/ingestion-history/actions/LoadEnhancedQc.js
@@ -5,6 +5,7 @@ import { LOAD_ENHANCED_QC_REPORT } from 'api'
 import { Button, Modal, Select } from 'antd'
 import { ExclamationCircleOutlined } from '@ant-design/icons'
 import styled from 'styled-components'
+import { months, getYears } from '../helper'
 
 const { Option } = Select
 
@@ -18,26 +19,26 @@ const LoadEnhancedQc = ({ selectedRecords, setSelectedRecords, refetch }) => {
 	const globalState = useContext(store)
 	const { state } = globalState
 	const { clientName } = state
-	const [loadQc, { loading }] = useMutation(LOAD_ENHANCED_QC_REPORT, {
-		onCompleted: () => {
-			setSelectedRecords([])
-			setVisible(false)
-			showSuccess()
-			refetch()
-			setYear(null)
-			setMonth(null)
-		}
-	})
 	const [visible, setVisible] = useState(false)
 	const [year, setYear] = useState(null)
 	const [month, setMonth] = useState(null)
-	const type =
-		selectedRecords.length > 0 ? selectedRecords[0].type.toLowerCase() : null
+
+	const [loadQc, { loading }] = useMutation(LOAD_ENHANCED_QC_REPORT, {
+		onCompleted: () => {
+			setVisible(false)
+			setSelectedRecords([])
+			clearInputs()
+			showSuccess()
+			refetch()
+		}
+	})
+
+	const type = selectedRecords.length
+		? selectedRecords[0].type.toLowerCase()
+		: null
 
 	const toggleModal = () => {
 		setVisible(!visible)
-		setYear(null)
-		setMonth(null)
 	}
 
 	const onOk = async () => {
@@ -48,22 +49,18 @@ const LoadEnhancedQc = ({ selectedRecords, setSelectedRecords, refetch }) => {
 			(obj) => obj.type === selectedRecords[0].type
 		)
 		if (!equalTypes) {
-			Modal.error({
-				title: 'Error in Loading',
-				content:
-					'You must select files for DPM or Sourcing. You cannot complete the Load action for both at the same time'
-			})
-			setYear(null)
-			setMonth(null)
+			showError(
+				'You must select files for DPM or Sourcing. You cannot complete the Load action for both at the same time'
+			)
 			toggleModal()
+			clearInputs()
 		} else {
 			try {
 				await loadQc({
 					variables: { jobIngestionIds, type, year, month }
 				})
 			} catch (e) {
-				error(e.message)
-				console.error('Error in loading for enhanced QC ', e)
+				showError(e.message)
 			}
 		}
 	}
@@ -77,44 +74,17 @@ const LoadEnhancedQc = ({ selectedRecords, setSelectedRecords, refetch }) => {
 		})
 	}
 
-	const error = (error) => {
+	const showError = (error) => {
 		Modal.error({
 			title: 'Error in Loading',
 			content: error
 		})
 	}
 
-	const handleYearChange = (year) => setYear(year)
-
-	const getYears = () => {
-		const current = new Date().getFullYear()
-		return [
-			current - 5,
-			current - 4,
-			current - 3,
-			current - 2,
-			current - 1,
-			current,
-			current + 1
-		]
+	const clearInputs = () => {
+		setYear(null)
+		setMonth(null)
 	}
-
-	const handleMonthChange = (month) => setMonth(month)
-
-	const months = [
-		{ label: 'January', value: 1 },
-		{ label: 'February', value: 2 },
-		{ label: 'March', value: 3 },
-		{ label: 'April', value: 4 },
-		{ label: 'May', value: 5 },
-		{ label: 'June', value: 6 },
-		{ label: 'July', value: 7 },
-		{ label: 'August', value: 8 },
-		{ label: 'September', value: 9 },
-		{ label: 'October', value: 10 },
-		{ label: 'November', value: 11 },
-		{ label: 'December', value: 12 }
-	]
 
 	return (
 		<>
@@ -158,8 +128,24 @@ const LoadEnhancedQc = ({ selectedRecords, setSelectedRecords, refetch }) => {
 						})}
 					</ul>
 					<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+						{type === 'dpm' && (
+							<Select
+								onChange={(month) => setMonth(month)}
+								style={{ width: 225 }}
+								placeholder={'Select month'}
+								value={month}
+							>
+								{months.map((month, i) => {
+									return (
+										<Option key={'month' + i} value={month.value}>
+											{month.label}
+										</Option>
+									)
+								})}
+							</Select>
+						)}
 						<Select
-							onChange={handleYearChange}
+							onChange={(year) => setYear(year)}
 							style={{ width: 225 }}
 							placeholder={'Select year'}
 							value={year}
@@ -174,22 +160,6 @@ const LoadEnhancedQc = ({ selectedRecords, setSelectedRecords, refetch }) => {
 									)
 								})}
 						</Select>
-						{type === 'dpm' && (
-							<Select
-								onChange={handleMonthChange}
-								style={{ width: 225 }}
-								placeholder={'Select month'}
-								value={month}
-							>
-								{months.map((month, i) => {
-									return (
-										<Option key={'month' + i} value={month.value}>
-											{month.label}
-										</Option>
-									)
-								})}
-							</Select>
-						)}
 					</div>
 				</>
 			</Modal>
