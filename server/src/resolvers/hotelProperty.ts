83789141cb28315e3cb8231bad681fcec3511c3e
Hanseul Shim <hanseulshim@gmail.com>
2020-06-19 14:34:40 -0400

    UPdate hotelPropertyList search

--- server/src/resolvers/hotelProperty.ts
+++ server/src/resolvers/hotelProperty.ts
@@ -24,53 +24,81 @@ export default {
 		): Promise<HotelPropertyType> => {
 			const LIMIT = 25
 			const OFFSET = Math.max(0, +pageNumber - 1) * LIMIT
-			const [{ count }] = await HotelPropertyView.query(hotel)
-				.skipUndefined()
-				.where('id', id)
-				.andWhere('hotelName', 'ILIKE', `%${hotelName || ''}%`)
-				.andWhere('hotelChainName', 'ILIKE', `%${hotelChainName || ''}%`)
-				.andWhere('address1', 'ILIKE', `%${address1 || ''}%`)
-				.andWhere('cityName', 'ILIKE', `%${cityName || ''}%`)
-				.andWhere('stateCode', 'ILIKE', `%${stateCode || ''}%`)
-				.andWhere('countryName', 'ILIKE', `%${countryName || ''}%`)
-				.andWhere('lanyonId', 'ILIKE', `%${lanyonId || ''}%`)
-				.andWhere('sabrePropertyId', 'ILIKE', `%${sabrePropertyId || ''}%`)
-				.andWhere('apolloPropertyId', 'ILIKE', `%${apolloPropertyId || ''}%`)
-				.andWhere('amadeusPropertyId', 'ILIKE', `%${amadeusPropertyId || ''}%`)
-				.andWhere(
+			const query = HotelPropertyView.query(hotel)
+				.select()
+				.orderBy('hotelName')
+				.offset(OFFSET)
+				.limit(LIMIT)
+				.orderBy('hotelName')
+			const countQuery = HotelPropertyView.query(hotel).select().count()
+			if (id) {
+				query.where('id', id)
+				countQuery.where('id', id)
+			}
+			if (hotelName) {
+				query.where('hotelName', 'ILIKE', `%${hotelName}%`)
+				countQuery.where('hotelName', 'ILIKE', `%${hotelName}%`)
+			}
+			if (hotelChainName) {
+				query.where('hotelChainName', 'ILIKE', `%${hotelChainName}%`)
+				countQuery.where('hotelChainName', 'ILIKE', `%${hotelChainName}%`)
+			}
+			if (address1) {
+				query.where(function () {
+					this.where('address1', 'ILIKE', `%${address1}%`).orWhere(
+						'address2',
+						'ILIKE',
+						`%${address1}%`
+					)
+				})
+				countQuery.where(function () {
+					this.where('address1', 'ILIKE', `%${address1}%`).orWhere(
+						'address2',
+						'ILIKE',
+						`%${address1}%`
+					)
+				})
+			}
+			if (cityName) {
+				query.where('cityName', 'ILIKE', `%${cityName}%`)
+				countQuery.where('cityName', 'ILIKE', `%${cityName}%`)
+			}
+			if (stateCode) {
+				query.where('stateCode', 'ILIKE', `%${stateCode}%`)
+				countQuery.where('stateCode', 'ILIKE', `%${stateCode}%`)
+			}
+			if (countryName) {
+				query.where('countryName', 'ILIKE', `%${countryName}%`)
+				countQuery.where('countryName', 'ILIKE', `%${countryName}%`)
+			}
+			if (lanyonId) {
+				query.where('lanyonId', 'ILIKE', `%${lanyonId}%`)
+				countQuery.where('lanyonId', 'ILIKE', `%${lanyonId}%`)
+			}
+			if (sabrePropertyId) {
+				query.where('sabrePropertyId', 'ILIKE', `%${sabrePropertyId}%`)
+				countQuery.where('sabrePropertyId', 'ILIKE', `%${sabrePropertyId}%`)
+			}
+			if (apolloPropertyId) {
+				query.where('apolloPropertyId', 'ILIKE', `%${apolloPropertyId}%`)
+				countQuery.where('apolloPropertyId', 'ILIKE', `%${apolloPropertyId}%`)
+			}
+			if (amadeusPropertyId) {
+				query.where('amadeusPropertyId', 'ILIKE', `%${amadeusPropertyId}%`)
+				countQuery.where('amadeusPropertyId', 'ILIKE', `%${amadeusPropertyId}%`)
+			}
+			if (worldspanPropertyId) {
+				query.where('worldspanPropertyId', 'ILIKE', `%${worldspanPropertyId}%`)
+				countQuery.where(
 					'worldspanPropertyId',
 					'ILIKE',
-					`%${worldspanPropertyId || ''}%`
+					`%${worldspanPropertyId}%`
 				)
-				.count()
-
+			}
+			const [{ count }] = await countQuery
 			return {
 				recordCount: +count,
-				data: await HotelPropertyView.query(hotel)
-					.skipUndefined()
-					.where('id', id)
-					.andWhere('hotelName', 'ILIKE', `%${hotelName || ''}%`)
-					.andWhere('hotelChainName', 'ILIKE', `%${hotelChainName || ''}%`)
-					.andWhere('address1', 'ILIKE', `%${address1 || ''}%`)
-					.andWhere('cityName', 'ILIKE', `%${cityName || ''}%`)
-					.andWhere('stateCode', 'ILIKE', `%${stateCode || ''}%`)
-					.andWhere('countryName', 'ILIKE', `%${countryName || ''}%`)
-					.andWhere('lanyonId', 'ILIKE', `%${lanyonId || ''}%`)
-					.andWhere('sabrePropertyId', 'ILIKE', `%${sabrePropertyId || ''}%`)
-					.andWhere('apolloPropertyId', 'ILIKE', `%${apolloPropertyId || ''}%`)
-					.andWhere(
-						'amadeusPropertyId',
-						'ILIKE',
-						`%${amadeusPropertyId || ''}%`
-					)
-					.andWhere(
-						'worldspanPropertyId',
-						'ILIKE',
-						`%${worldspanPropertyId || ''}%`
-					)
-					.offset(OFFSET)
-					.limit(LIMIT)
-					.orderBy('hotelName')
+				data: await query
 			}
 		}
 	}
